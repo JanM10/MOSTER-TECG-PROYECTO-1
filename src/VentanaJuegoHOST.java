a84e88356a48f4f9f30838462e83715023447e86
JanM10 <JanM10@users.noreply.github.com>
2020-11-02 23:53:53 -0600

    Se cambio el nobre de la clase ya que este clase va a ser para inciar como HOST del juego y va a tener un puerto especifico.

--- src/VentanaJuegoHOST.java
+++ src/VentanaJuegoHOST.java
@@ -1,11 +1,11 @@
 import javax.swing.JOptionPane;
 
-public class VentanaJuego extends javax.swing.JFrame {
+public class VentanaJuegoHOST extends javax.swing.JFrame {
 
     /**
-     * Creates new form VentanaJuego
+     * Creates new form VentanaJuegoHOST
      */
-    public VentanaJuego() {
+    public VentanaJuegoHOST() {
         initComponents();
     }
 
@@ -108,20 +108,20 @@ public class VentanaJuego extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VentanaJuego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaJuegoHOST.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VentanaJuego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaJuegoHOST.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VentanaJuego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaJuegoHOST.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VentanaJuego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VentanaJuegoHOST.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new VentanaJuego().setVisible(true);
+                new VentanaJuegoHOST().setVisible(true);
             }
         });
     }
